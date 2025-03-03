8b2de802089e50d14bd08622c504669df2abcc62
Lewis Thackeray <lewisthackeray123@outlook.com>
2025-03-03 12:23:17 +0000

    Creating the Download Interface

--- src/optionsScreen.jsx
+++ src/optionsScreen.jsx
@@ -1,11 +1,18 @@
-import React, {useEffect, useState} from 'react'; import './optionsScreen.css';
+import React, {useEffect, useState} from 'react'; import { MdOutlineFileDownload } from "react-icons/md"; import './optionsScreen.css';
 
-// Functional Component to Create the Options Screen which is Displayed when Reverse Geocoding is Complete.
 const OptionsScreen = () => {
   return (
     <div className="options-container">
-      <h1>Your Address List has been Created. What Next?</h1> <div className="buttons-container">
-        <button>Download Address List</button> <button>Create Letters/Postcards</button>
+      <h1 className="options-title">Your Address List has been Created. What's Next?</h1>
+      <div className="buttons-container">
+        <div className="option-box">
+          <div className="download-content">
+            <MdOutlineFileDownload className="download-icon" />
+            <h2>Download Address List</h2> <p>Select the File Type you would like to Download the Address List as:</p>
+            <div className="download-content-buttons-group"> <button>.xlsx</button> <button>.xls</button> <button>.csv</button> </div>
+          </div>
+        </div>
+        <div className="option-box">Option 2</div>
       </div>
     </div>
   );
