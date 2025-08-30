c35a84f4e0e714ce4e217a66a93a3715f8de1845
Lewis Thackeray <lewisthackeray123@outlook.com>
2025-08-30 15:10:02 +0100

    Fix vite.config.js and Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/VoriumDemo/',
   server: {
     port: 5190, // Change this to the port you want
   },
